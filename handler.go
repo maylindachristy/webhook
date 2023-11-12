0d17089c4429bdf8a5a14dfbea3eded5d6818e0c
maylindachristy <maylindachristy@gmail.com>
2023-11-12 21:32:03 +0700

    update

---
+++ handler.go
@@ -0,0 +1,40 @@
+package webhook
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aiteung/atapi"
+	"github.com/aiteung/atmessage"
+	"github.com/aiteung/module/model"
+	"github.com/whatsauth/wa"
+	"github.com/whatsauth/ws"
+	"net/http"
+	"os"
+)
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	var WAKeyword = "wh4t5auth0"
+	var msg model.IteungMessage
+	var resp atmessage.Response
+	json.NewDecoder(r.Body).Decode(&msg)
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		if ws.IsLoginRequest(msg, WAKeyword) { //untuk whatsauth request login
+			dt := &ws.WhatsauthRequest{
+				Uuid:        ws.GetUUID(msg, WAKeyword),
+				Phonenumber: msg.Phone_number,
+				Delay:       msg.From_link_delay,
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/whatsauth/request")
+		} else { //untuk membalas pesan masuk
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: "Hai friend " + msg.Alias_name,
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+		}
+	} else {
+		resp.Response = "Secret Salah"
+	}
+	fmt.Fprintf(w, resp.Response)
+}
